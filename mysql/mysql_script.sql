76b5be55f41e6bead531396326ee0216b2a12f0a
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-02-19 18:46:36 +0530

    Update mysql_script.sql

--- mysql/mysql_script.sql
+++ mysql/mysql_script.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE IF NOT EXISTS employees_db;
+CREATE DATABASE employees_db;
 USE employees_db;
-CREATE TABLE IF NOT EXISTS EMPLOYEES (
+CREATE TABLE  EMPLOYEES (
     emp_id INT AUTO_INCREMENT PRIMARY KEY,
     emp_name VARCHAR(50) NOT NULL,
      emp_contact INT,
     emp_add VARCHAR(250),
 );
-INSERT INTO EMPLOYEES (emp_name, emp_contact, emp_add)
+INSERT INTO EMPLOYEES (emp_id,emp_name, emp_contact, emp_add)
 VALUES
-    ('Jagriti', 7007220804, "Gorakhpur"),
-        ('Saloni', 7007220805, "Lucknow"),
-       ('Kajal', 7007220889, "sant kabir nagar");
+    (1,'Jagriti', 7007220804, "Gorakhpur"),
+        (2,'Saloni', 7007220805, "Lucknow"),
+       (3,'Kajal', 7007220889, "sant kabir nagar");
